da8b82477d3676e64bc5cc6a7ef933c1d72468b8
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-23 21:10:28 +0900

    React 코딩애플 #5 state 변경 ~7:20

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [title, chtitle] = useState(['강남 우동 맛집', '남자 코트 추천', '우리집 변기 과연 괜찮을까?']);
+    let [number, chnumber] = useState(0);
+    let changeCss = {
+        color: `aqua`,
+        fontSize: `35px`
+    }
+
+    function changeTitle() { 
+        let newArray = [...title];
+        newArray[1] = '여자'
+      }
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <div style={changeCss}>개발 Blog</div>
+            </div>
+            <button onClick={changeTitle}>버튼</button>
+            <div className="list">
+                <h3>{title[0]}
+                    <span
+                        onClick={() => {
+                            chnumber(number + 1)
+                        }}>❤</span>
+                    {number}</h3>
+                <p>2월 17일 발행</p>
+                <hr/>
+            </div>
+            <div className="list">
+                <h3>{title[1]}</h3>
+                <p>2월 18일 발행</p>
+                <hr/>
+            </div>
+            <div className="list">
+                <h3>{title[2]}</h3>
+                <p>2월 19일 발행</p>
+                <hr/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
