d99951bce2e2356ad30175b2de622c2e4729d7d3
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-24 19:51:17 +0900

    3/24 생활코딩 #5 props

--- sanghwalcoding/src/App.js
+++ sanghwalcoding/src/App.js
@@ -1,43 +1,59 @@
+import React from 'react'
 import './App.css';
-function Header() {
+function Header(props) {
+    console.log('props', props, props.title);
     return (
         <header>
             <h1>
-                <a href="/">WEB</a>
+                <a href="/">{props.title}</a>
             </h1>
         </header>
     )
 }
-function Nav() {
+
+function Nav(props) {
+    const lis = []
+        for(let i = 0 ; i < props.topics.length;i++){
+          let t = props.topics[i]; 
+          lis.push(<li key={t.id}><a href={'/read/'+t.id}>t.title</a></li>);
+        }
     return (
-        <nav>
-            <ol>
-                <li>
-                    <a href="/read/1">html</a>
-                </li>
-                <li>
-                    <a href="/read/2">css</a>
-                </li>
-                <li>
-                    <a href="/read/3">js</a>
-                </li>
-            </ol>
-        </nav>
-    )
+    <nav>
+        <ol>
+          { lis }
+        </ol>
+    </nav>)
 }
-function Article(){
-  return(
-    <article>
-    <h2>Welcome</h2>
-     Hello, WEB
-     </article>
-  )
+function Article() {
+    return (<article>
+        <h2>Welcome</h2>
+        Hello, WEB
+    </article>)
 }
 function App() {
+    const topics = [ // eslint-disable-line no-unused-vars
+        {
+          id: 1,
+          title: 'html',
+          body: 'html is....'
+        }
+        ,
+        {
+          id: 2,
+          title: 'css',
+          body: 'css is....'
+        }
+        ,
+        {
+          id: 3,
+          title: 'js',
+          body: 'js is....'
+        }
+    ];
     return (
         <div className="App">
-            <Header></Header>
-            <Nav></Nav>
+            <Header title="React" onClick = {(e)=>{e.preventDefault()}}></Header>
+            <Nav topics={topics}></Nav>
             <Article></Article>
         </div>
     );
