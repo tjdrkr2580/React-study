a87c90cf5f22b67a9c34a615c7a5bb155af1b2bd
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-24 21:01:45 +0900

    3/24 생활코딩 #6 이벤트

--- sanghwalcoding/src/App.js
+++ sanghwalcoding/src/App.js
@@ -5,7 +5,7 @@ function Header(props) {
     return (
         <header>
             <h1>
-                <a href="/">{props.title}</a>
+                <a href="/" onClick = {(e)=>{e.preventDefault(); props.onChangeMode();}}>{props.title}</a>
             </h1>
         </header>
     )
@@ -15,7 +15,7 @@ function Nav(props) {
     const lis = []
         for(let i = 0 ; i < props.topics.length;i++){
           let t = props.topics[i]; 
-          lis.push(<li key={t.id}><a href={'/read/'+t.id}>t.title</a></li>);
+          lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(e)=>{e.preventDefault(); props.onChangeMode(e.target.id);}}>t.title</a></li>);
         }
     return (
     <nav>
@@ -52,8 +52,8 @@ function App() {
     ];
     return (
         <div className="App">
-            <Header title="React" onClick = {(e)=>{e.preventDefault()}}></Header>
-            <Nav topics={topics}></Nav>
+            <Header title="React" onChangeMode = {()=>{alert('header');}}></Header>
+            <Nav topics={topics} onChangeMode = {(id)=>{alert(id);}}></Nav>
             <Article></Article>
         </div>
     );
