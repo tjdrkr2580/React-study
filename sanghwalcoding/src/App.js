5a467b1e0510f08636beaffb4b02e192dc03880f
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-28 11:15:06 +0900

    3/28 생활코딩 #7 state

--- sanghwalcoding/src/App.js
+++ sanghwalcoding/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { useState } from 'react';
 import './App.css';
+
 function Header(props) {
-    console.log('props', props, props.title);
     return (
         <header>
             <h1>
@@ -15,7 +16,7 @@ function Nav(props) {
     const lis = []
         for(let i = 0 ; i < props.topics.length;i++){
           let t = props.topics[i]; 
-          lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(e)=>{e.preventDefault(); props.onChangeMode(e.target.id);}}>t.title</a></li>);
+          lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(e)=>{e.preventDefault(); props.onChangeMode(Number(e.target.id));}}>t.title</a></li>);
         }
     return (
     <nav>
@@ -24,14 +25,15 @@ function Nav(props) {
         </ol>
     </nav>)
 }
-function Article() {
+function Article(props) {
     return (<article>
-        <h2>Welcome</h2>
-        Hello, WEB
+        <h2>{ props.title }</h2>
+        { props.body }
     </article>)
 }
 function App() {
-    const mode = 'WELCOME';
+    const [id,setId] = useState(null);
+    const [mode, setMode] = useState(null);
     const topics = [ // eslint-disable-line no-unused-vars
         {
           id: 1,
@@ -53,18 +55,25 @@ function App() {
     ];
     let content = null;
     if(mode === 'WELCOME'){
-
+        content = <Article title="Welcome" body="Hello, WEB"></Article>
     }
     else if(mode === 'READ'){
-
+        let title , body = null
+        for(let i = 0 ; i < topics.length ; i++){
+            if(topics[i].id === id){
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title={title} body={body}></Article>
     }
     return (
         <div className="App">
-            <Header title="React" onChangeMode = {()=>{alert('header');}}></Header>
-            <Nav topics={topics} onChangeMode = {(id)=>{alert(id);}}></Nav>
-            <Article></Article>
+            <Header title="React" onChangeMode = {()=>{setMode('WELCOME');}}></Header>
+            <Nav topics={topics} onChangeMode = {(_id)=>{setMode('READ'); setId(_id);}}></Nav>
+            {content}
         </div>
-    );
+    )
 }
 
 export default App;
