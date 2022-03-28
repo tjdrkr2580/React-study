03446a4667d73bc4a190272b259249ca5d2dbb7f
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-28 19:28:58 +0900

    3/28 생활코딩 #9 Update

--- sanghwalcoding/src/App.js
+++ sanghwalcoding/src/App.js
@@ -1,105 +1,220 @@
-import React from 'react'
-import { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-
-function Header(props) {
-    return (
-        <header>
-            <h1>
-                <a href="/" onClick = {(e)=>{e.preventDefault(); props.onChangeMode();}}>{props.title}</a>
-            </h1>
-        </header>
-    )
+import {useState} from 'react';
+function Article(props) {
+    return <article>
+        <h2>{props.title}</h2>
+        {props.body}
+    </article>
 }
-
+function Header(props) {
+    return <header>
+        <h1>
+            <a
+                href="/"
+                onClick={(event) => {
+                    event.preventDefault();
+                    props.onChangeMode();
+                }}>{props.title}</a>
+        </h1>
+    </header>
+}
 function Nav(props) {
     const lis = []
-        for(let i = 0 ; i < props.topics.length;i++){
-          let t = props.topics[i]; 
-          lis.push(<li key={t.id}><a id={t.id} href={'/read/'+t.id} onClick={(e)=>{e.preventDefault(); props.onChangeMode(Number(e.target.id));}}>{ t.title }</a></li>);
-        }
-    return (
-    <nav>
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a
+                    id={t.id}
+                    href={'/read/' + t.id}
+                    onClick={event => {
+                        event.preventDefault();
+                        props.onChangeMode(Number(event.target.id));
+                    }}>{t.title}</a>
+            </li>
+        )
+    }
+    return <nav>
         <ol>
-          { lis }
+            {lis}
         </ol>
-    </nav>)
+    </nav>
 }
-function Article(props) {
-    return (<article>
-        <h2>{ props.title }</h2>
-        { props.body }
-    </article>)
-}
-
-function Create(props){
+function Create(props) {
     return <article>
         <h2>Create</h2>
-        <form onSubmit={e =>{e.preventDefault();
-        const title = e.target.title.value;
-        const body = e.target.body.value;
-        props.onCreate(title, body);}}>
-        <p><input type="text" name='title' placeholder='title'/></p>
-        <p><textarea name="body" placeholder='body'></textarea></p>
-        <p><input type="submit" value="Create"/></p>
+        <form
+            onSubmit={event => {
+                event.preventDefault();
+                const title = event.target.title.value;
+                const body = event.target.body.value;
+                props.onCreate(title, body);
+            }}>
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p>
+                <textarea name="body" placeholder="body"></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Create"></input>
+            </p>
+        </form>
+    </article>
+}
+function Update(props) {
+    const [title, setTitle] = useState(props.title);
+    const [body, setBody] = useState(props.body);
+    return <article>
+        <h2>Update</h2>
+        <form
+            onSubmit={event => {
+                event.preventDefault();
+                const title = event.target.title.value;
+                const body = event.target.body.value;
+                props.onUpdate(title, body);
+            }}>
+            <p><input
+                type="text"
+                name="title"
+                placeholder="title"
+                value={title}
+                onChange={event => {
+            setTitle(event.target.value);
+        }}/></p>
+            <p>
+                <textarea
+                    name="body"
+                    placeholder="body"
+                    value={body}
+                    onChange={event => {
+                        setBody(event.target.value);
+                    }}></textarea>
+            </p>
+            <p>
+                <input type="submit" value="Update"></input>
+            </p>
         </form>
     </article>
 }
 function App() {
-    const [id,setId] = useState(null);
     const [mode, setMode] = useState('WELCOME');
-    const [nextId,setNextId] = useState(4);
-    const [topics,setTopics] = useState([ // eslint-disable-line no-unused-vars
+    const [id, setId] = useState(null);
+    const [nextId, setNextId] = useState(4);
+    const [topics, setTopics] = useState([
         {
-          id: 1,
-          title: 'html',
-          body: 'html is....'
+            id: 1,
+            title: 'html',
+            body: 'html is ...'
+        }, {
+            id: 2,
+            title: 'css',
+            body: 'css is ...'
+        }, {
+            id: 3,
+            title: 'javascript',
+            body: 'javascript is ...'
         }
-        ,
-        {
-          id: 2,
-          title: 'css',
-          body: 'css is....'
-        }
-        ,
-        {
-          id: 3,
-          title: 'js',
-          body: 'js is....'
-        }]);
+    ]);
     let content = null;
-    if(mode === 'WELCOME'){
+    let contextControl = null;
+    if (mode === 'WELCOME') {
         content = <Article title="Welcome" body="Hello, WEB"></Article>
-    }
-    else if(mode === 'READ'){
-        let title , body = null
-        for(let i = 0 ; i < topics.length ; i++){
-            if(topics[i].id === id){
+    } else if (mode === 'READ') {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            console.log(topics[i].id, id);
+            if (topics[i].id === id) {
                 title = topics[i].title;
                 body = topics[i].body;
             }
         }
         content = <Article title={title} body={body}></Article>
-    }
-    else if(mode === 'CREATE'){
-        content = <Create onCreate={(_title, _body)=>{
-            const newTopic = {id: nextId , title:_title, body:_body};
-            const newTopics = [...topics];
-            newTopics.push(newTopic);
-            setTopics(newTopics);
-            setMode('READ');
-            setId(nextId);
-            setNextId(nextId+1);
-        }}></Create>
+        contextControl = <li>
+            <a
+                href={'/update/' + id}
+                onClick={event => {
+                    event.preventDefault();
+                    setMode('UPDATE');
+                }}>Update</a>
+        </li>
+    } else if (mode === 'CREATE') {
+        content = <Create
+            onCreate={(_title, _body) => {
+                const newTopic = {
+                    id: nextId,
+                    title: _title,
+                    body: _body
+                }
+                const newTopics = [...topics]
+                newTopics.push(newTopic);
+                setTopics(newTopics);
+                setMode('READ');
+                setId(nextId);
+                setNextId(nextId + 1);
+            }}></Create>
+    } else if (mode === 'UPDATE') {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Update
+            title={title}
+            body={body}
+            onUpdate={(title, body) => {
+                console.log(title, body);
+                const newTopics = [...topics]
+                const updatedTopic = {
+                    id: id,
+                    title: title,
+                    body: body
+                }
+                for (let i = 0; i < newTopics.length; i++) {
+                    if (newTopics[i].id === id) {
+                        newTopics[i] = updatedTopic;
+                        break;
+                    }
+                }
+                setTopics(newTopics);
+                setMode('READ');
+            }}></Update>
     }
     return (
-        <div className="App">
-            <Header title="React" onChangeMode = {()=>{setMode('WELCOME');}}></Header>
-            <Nav topics={topics} onChangeMode = {(_id)=>{setMode('READ'); setId(_id);}}></Nav>
+        <div>
+            <Header
+                title="WEB"
+                onChangeMode={() => {
+                    setMode('WELCOME');
+                }}></Header>
+            <Nav
+                topics={topics}
+                onChangeMode={(_id) => {
+                    setMode('READ');
+                    setId(_id);
+                }}></Nav>
             {content}
-            <a href='/create' onClick={(e)=>{e.preventDefault(); setMode('CREATE'); console.log(topics);}}>Create</a>
+            <a
+                href="/create"
+                onClick={event => {
+                    event.preventDefault();
+                    setMode('CREATE');
+                }}>Create</a>
+            <ul>
+                <li>
+                    <a
+                        href="/create"
+                        onClick={event => {
+                            event.preventDefault();
+                            setMode('CREATE');
+                        }}>Create</a>
+                </li>
+                {contextControl}
+            </ul>
         </div>
-    )
+    );
 }
-
 export default App;
