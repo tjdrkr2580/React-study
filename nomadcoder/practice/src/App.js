923b60155546ef1676522e8c56b58fa53a52d8f4
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-03-31 20:49:15 +0900

    3/31 노마드 영화 웹 ~#3.7 State Practice part Two

--- nomadcoder/practice/src/App.js
+++ nomadcoder/practice/src/App.js
@@ -3,28 +3,37 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [minutes, setMiniutes] = useState("");
+    const [amount, setAmount] = useState("");
+    const [flipped, setFlipped] = useState(false);
     const onChange = (event) => {
-        setMiniutes(event.target.value);
+        setAmount(event.target.value);
     }
-    const reset = () => setMiniutes(0);
+    const reset = () => setAmount(0);
+    const onFilp = () => {reset(); setFlipped((current) => !current)};
     return (
         <div className="App">
             <h1 className="hi">Super Converter</h1>
             <div>
-                <label htmlFor='minutes'>Second</label>
+                <label htmlFor='amount'>Second</label>
                 <input
-                    value={minutes}
-                    id="minutes"
-                    placeholder='Minutes'
+                    value={flipped ? amount*60 : amount }
+                    id="amount"
+                    placeholder='amount'
                     type="number"
-                    onChange={onChange} />
-                <h4>You want to convert {minutes}</h4>
+                    onChange={onChange}
+                    disabled={flipped}/>
+                <h4>You want to convert {amount}</h4>
             </div>
             <div>
-                <label htmlFor='hours'>Minutes</label>
-                <input id="hours" value={minutes/60} placeholder='Hours' type="number" disabled/>
-                <button onClick={ reset }>Reset</button>
+                <label htmlFor='hours'>hour</label>
+                <input
+                    id="hours"
+                    value={flipped ? amount : amount / 60}
+                    placeholder='Hours'
+                    type="number"
+                    disabled={!flipped}/>
+                <button onClick={reset}>Reset</button>
+                <button onClick={onFilp}>flip</button>
             </div>
         </div>
     );
