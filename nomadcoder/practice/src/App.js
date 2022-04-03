8ded33c90f62fa1fadf7698844ee7d4f3b2c6e69
Wonsi <tjdrkr2580@naver.com>
2022-04-03 23:20:51 +0900

    4/3 노마드코더 리액트 영화 웹 ~#3.9 Final Practice and Recap ~ing

--- nomadcoder/practice/src/App.js
+++ nomadcoder/practice/src/App.js
@@ -9,13 +9,18 @@ function MinutesToHours() {
         setAmount(event.target.value);
     }
     const reset = () => setAmount(0);
-    const onFilp = () => {reset(); setFlipped((current) => !current)};
+    const onFilp = () => {
+        reset();
+        setFlipped((current) => !current)
+    };
     return (
         <div className="App">
             <div>
                 <label htmlFor='amount'>Second</label>
                 <input
-                    value={flipped ? amount*60 : amount }
+                    value={flipped
+                        ? amount * 60
+                        : amount}
                     id="amount"
                     placeholder='amount'
                     type="number"
@@ -27,20 +32,46 @@ function MinutesToHours() {
                 <label htmlFor='hours'>hour</label>
                 <input
                     id="hours"
-                    value={flipped ? amount : amount / 60}
+                    value={flipped
+                        ? amount
+                        : amount / 60}
                     placeholder='Hours'
                     type="number"
                     disabled={!flipped}/>
                 <button onClick={reset}>Reset</button>
-                <button onClick={onFilp}>{flipped ? "turn back" : "flip"}</button>
+                <button onClick={onFilp}>{
+                        flipped
+                            ? "turn back"
+                            : "flip"
+                    }</button>
             </div>
         </div>
     );
 }
 
-function KmToMiles(){
+function KmToMiles() {
+    const [amount,setAmount] = useState(0);
+    const [flip,setFlip] = useState(false);
+    const onFilp = () => {
+        console.log(flip);
+        setFlip((current) => !current);
+    }
+    const reset = () => setAmount(0);
+
     return (
-        <h3>KM 2 Mile</h3>
+        <div className="app">
+            <h3>KM 2 Mile</h3>
+            <p>
+                <label htmlFor='km'>Km :
+                </label><input id='km' type="number" disabled={flip}/></p>
+            <h4>You want to convert</h4>
+            <p>
+                <label htmlFor='km'>Mile:
+                </label><input id='mile' type="number" disabled={!flip}/>
+                <button>reset</button>
+                <button onClick={onFilp}>flip</button>
+            </p>
+        </div>
     )
 }
 
@@ -52,18 +83,28 @@ function App() {
     return (
         <div className="App">
             <h1 className="hi">Super Converter</h1>
-            <select value = {index} onChange={onSelect}>
+            <select value={index} onChange={onSelect}>
                 <option value="xx">Select your units</option>
                 <option value="0">MinutesToHours</option>
                 <option value="1">Km & Miles</option>
             </select>
-            {index === "xx" ? "Please Select you units" : null}
-            {index === "0" ? <MinutesToHours/> : null}
-            {index === "1" ? <KmToMiles/> : null}
+            {
+                index === "xx"
+                    ? "Please Select you units"
+                    : null
+            }
+            {
+                index === "0"
+                    ? <MinutesToHours/>
+                    : null
+            }
+            {
+                index === "1"
+                    ? <KmToMiles/>
+                    : null
+            }
         </div>
     );
 }
-
-
 
 export default App;
