c93698e4d54440d3aa24249e5bfec27ab085da2e
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-06 20:51:50 +0900

    4/6 노마드코더 리액트 영화 웹 ~#6.2 Deps

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState , useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from "./Button";
 import styles from "./App.module.css";
 ;
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onChange = (e) => {setKeyword(e.target.value);}
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  const iRunOnlyOne = () => {
-    console.log("i run only one");
-  }
-    useEffect(iRunOnlyOne,[]);
+  useEffect(() => {
+    if(keyword !== '' && keyword.length > 5)
+    console.log("search for " , keyword);
+  },[keyword]);
+  useEffect(() => {
+    if(counter !== '0' && counter > 5)
+    console.log("my counter is " , counter);
+  },[counter]);
   return (
     <div>
+        <input type="text" value={keyword} onChange={onChange} placeholder='Search here...'/>
         <h1>{counter}</h1>
-        <button onClick={onClick}>click me!</button>
+        <button  onClick={onClick}>click me!</button>
     </div>
   );
 }
