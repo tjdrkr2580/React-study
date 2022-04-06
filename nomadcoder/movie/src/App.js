996a4b375503d0eac5f490bede27d66fa97f90ad
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-06 19:50:06 +0900

    4/6 노마드코더 리액트 영화 웹 ~#6.0 Introduction

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,11 +1,15 @@
+import React, { useState } from 'react';
 import Button from "./Button";
 import styles from "./App.module.css";
 ;
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("render");
   return (
     <div>
-      <h1 className={styles.title}>Welcome! React</h1>
-      <Button text={"Continue"}></Button>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me!</button>
     </div>
   );
 }
