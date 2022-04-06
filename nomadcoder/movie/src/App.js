2de663845e763362836e69771fa6281ae8ab0d0c
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-06 20:04:58 +0900

    4/6 노마드코더 리액트 영화 웹 ~#6.1 useEffect

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import Button from "./Button";
 import styles from "./App.module.css";
 ;
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+  const iRunOnlyOne = () => {
+    console.log("i run only one");
+  }
+    useEffect(iRunOnlyOne,[]);
   return (
     <div>
         <h1>{counter}</h1>
