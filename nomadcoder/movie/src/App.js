c2389c5bc2b3a27cd34235c20212f46ea36435a3
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-04-10 02:09:37 +0900

    4/10 노마드코더 리액트 영화 웹 ~#7.2 Coin Tracker

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,36 +1,11 @@
 import React, {useState} from 'react';
 
 function App() {
-    const [toDo, setTodo] = useState("");
-    const [toDos, setTodos] = useState([]);
-    const onChange = (event) => {
-        setTodo(event.target.value);
-    }
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setTodos(currentArray => [
-            ...currentArray , toDo
-        ]);
-        setTodo("");
-    };
+  const [loading,setLoading] = useState(true);
     return (
         <div>
-            <h1>My Todo Count {toDos.length}</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    value={toDo}
-                    onChange={onChange}
-                    type="text"
-                    placeholder='Write your to do'/>
-                <button>Add to do</button>
-            </form>
-            <hr/>
-            <ul>
-                {toDos.map((item, index) => <li key={index}>{item}</li>)}
-            </ul>
+          <h1>The Coins!</h1>
+          {loading ? <strong>Loading...</strong> : null}
         </div>
     );
 }
