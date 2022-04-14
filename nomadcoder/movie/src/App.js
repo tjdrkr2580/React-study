61b0360cb2e3fc3da536396ecfb442dd223d2d82
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-14 12:14:36 +0900

    4/14 노마드코더 리액트 영화 웹 ~#7.2 Coin Tracker

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,18 +1,39 @@
-import React, {useState , useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  const [loading,setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() =>{
-    fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json())
-    .then((json) => setCoins(json));
-    setLoading(false);  
-  },[])
-  
+    const [loading, setLoading] = useState(false);
+    const [coins, setCoins] = useState([]);
+    const [dollar, setDollar] = useState(0);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then(
+                response => response.json()
+            )
+            .then((json) => setCoins(json));
+        setLoading(true);
+    }, [])
+
     return (
         <div>
-          <h1>The Coins!</h1>
-          {loading ? <strong>Loading...</strong> : null}
+            <h1>The Coins! {coins.length}</h1>
+            {
+                loading
+                    ? null
+                    : <strong>Loading...</strong>
+            }
+
+            <select>
+
+                {
+                    coins.map(
+                        (coin) =>< option > {
+                            coin.name
+                        }({coin.symbol} : $ {
+                            coin.quotes.USD.price
+                        })</option>
+                    )
+                }
+            </select>
         </div>
     );
 }
