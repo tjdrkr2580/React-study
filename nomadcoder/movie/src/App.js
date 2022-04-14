fd63d3803f88d8f64b9533da3f474ad994b2df5f
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-14 19:07:20 +0900

    Setting movie

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,41 +1,7 @@
 import React, {useState, useEffect} from 'react';
 
 function App() {
-    const [loading, setLoading] = useState(false);
-    const [coins, setCoins] = useState([]);
-    const [dollar, setDollar] = useState(0);
-    useEffect(() => {
-        fetch("https://api.coinpaprika.com/v1/tickers")
-            .then(
-                response => response.json()
-            )
-            .then((json) => setCoins(json));
-        setLoading(true);
-    }, [])
-
-    return (
-        <div>
-            <h1>The Coins! {coins.length}</h1>
-            {
-                loading
-                    ? null
-                    : <strong>Loading...</strong>
-            }
-
-            <select>
-
-                {
-                    coins.map(
-                        (coin) =>< option > {
-                            coin.name
-                        }({coin.symbol} : $ {
-                            coin.quotes.USD.price
-                        })</option>
-                    )
-                }
-            </select>
-        </div>
-    );
+      <div></div>
 }
 
 export default App;
