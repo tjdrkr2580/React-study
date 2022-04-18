9efbd665d35a6e24b5006c925f66cdb6ad51f75c
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-04-18 19:48:22 +0900

    4/18 kip

--- nomadcoder/movie/src/App.js
+++ nomadcoder/movie/src/App.js
@@ -1,7 +1,18 @@
 import React, {useState, useEffect} from 'react';
-
 function App() {
-      <div></div>
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`)
+            .then(
+                (response) => response.json()
+            )
+            .then(json => console.log(json));
+    })
+    return <div>{
+            loading
+                ? <h1>Loading...</h1>
+                : null
+        }</div>
 }
 
 export default App;
