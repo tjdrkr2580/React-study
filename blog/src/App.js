abb0a374573f6724a0890da32e573c7bc3b0e969
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-05-16 01:14:14 +0900

    #5 : state가 array/object면

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,26 +3,37 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-    let post = '강남 우동 맛집';
-    let [store,chstore] = useState(['남자코트 추천','인천 김치찌개','청담동 라면']);
-    let [logo,chlogo] = useState(['React blog']);
-    let [count,changeCount] = useState(0);
+    let [글제목, 글제목변경] = useState(['강자코트 추천', '아천 김치찌개', '나담동 라면']);
+    let [logo, chlogo] = useState(['React blog']);
+    let [count, changeCount] = useState(0);
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>{logo}</h4>
             </div>
+
+            <button
+                onClick={() => {
+                    let copy = [...글제목];
+                    copy.sort();
+                    글제목변경(copy);
+                }}>가나다순</button>
             <div className="list">
-                <h4>{store[0]} <span onClick={()=>{changeCount(count+1)}}> 👍 </span>{count}</h4>
-                <button onClick={()=>{chstore(['여자코트 추천','인천 김치찌개','청담동 라면'])}}>버튼</button>
+                <h4>{글제목[0]}
+                    <span
+                        onClick={() => {
+                            changeCount(count + 1)
+                        }}>
+                        👍
+                    </span>{count}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{store[1]}</h4>
+                <h4>{글제목[1]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{store[2]}</h4>
+                <h4>{글제목[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
         </div>
