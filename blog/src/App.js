b4b9dc17e6352ca2b4cd17cfe9e086d2ff28a67c
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-05-15 23:45:38 +0900

    #2 JSX 문법은 3개가 다임

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let post = '강남 우동 맛집';
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4 style ={{color : 'red' , fontSize : '16px'}}>블로그임</h4>
+            </div>
+            <h4>{post}</h4>
+        </div>
+    );
 }
 
 export default App;
