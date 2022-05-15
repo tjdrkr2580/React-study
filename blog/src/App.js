35f10972874a2883c527c8dfc12d9692a4b16580
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-05-16 01:37:09 +0900

    #6. div 너무 많으면 Component

--- blog/src/App.js
+++ blog/src/App.js
@@ -36,8 +36,21 @@ function App() {
                 <h4>{글제목[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
+            <Modal></Modal>
         </div>
     );
+}
+
+function Modal() {
+    return (
+      <>
+        <div className="modal">
+            <h4>제목</h4>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+      </>
+    )
 }
 
 export default App;
