028309780631efff03aea45ad1c90a1a6a76d2a9
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-05-16 00:02:40 +0900

    #3 state 쓰면 뭐가 좋냐면

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  let post = '강남 우동 맛집';
+    let post = '강남 우동 맛집';
+    let [logo,chlogo] = useState('React blog');
     return (
         <div className="App">
             <div className="black-nav">
-                <h4 style ={{color : 'red' , fontSize : '16px'}}>블로그임</h4>
+                <h4>{logo}</h4>
             </div>
-            <h4>{post}</h4>
+            <div className="list">
+                <h4>글제목</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>글제목</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>글제목</h4>
+                <p>2월 17일 발행</p>
+            </div>
         </div>
     );
 }
