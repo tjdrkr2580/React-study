c93eeef42caab8e3abc141fc6f444cbb9531d4b1
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-05-16 00:24:20 +0900

    #4. 버튼에 지리는 기능만들기

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,22 +4,25 @@ import {useState} from 'react';
 
 function App() {
     let post = '강남 우동 맛집';
-    let [logo,chlogo] = useState('React blog');
+    let [store,chstore] = useState(['남자코트 추천','인천 김치찌개','청담동 라면']);
+    let [logo,chlogo] = useState(['React blog']);
+    let [count,changeCount] = useState(0);
     return (
         <div className="App">
             <div className="black-nav">
                 <h4>{logo}</h4>
             </div>
             <div className="list">
-                <h4>글제목</h4>
+                <h4>{store[0]} <span onClick={()=>{changeCount(count+1)}}> 👍 </span>{count}</h4>
+                <button onClick={()=>{chstore(['여자코트 추천','인천 김치찌개','청담동 라면'])}}>버튼</button>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>글제목</h4>
+                <h4>{store[1]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>글제목</h4>
+                <h4>{store[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
         </div>
