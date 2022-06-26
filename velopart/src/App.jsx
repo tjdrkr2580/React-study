b3196a1048eedc728071138189c73c1678fc27f0
KIMTAEHYUN <tjdrkr2580@naver.com>
2022-06-27 01:24:42 +0900

    create UserList.jsx : 11.렌더링 배열하기

--- velopart/src/App.jsx
+++ velopart/src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css';
-import Counter from './Counter';
-import Hello from './Hello';
 import InputSample from './InputSample';
 
 function App() {
   return (
     <>
-    <Hello name="ririri" isSpecial={true}/>
-    <Hello />
-    <Counter />
     <InputSample/>
     </>
   );
--- velopart/src/InputSample.jsx
+++ velopart/src/InputSample.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
-import { useState } from 'react';
+import {React, useState , useRef} from 'react';
 
 const InputSample = () => {
-    const [text,setText] = useState("");
-  return (
-    <div>
-        <input type="text" onChange={(e)=>{setText(e.target.value);}} />
-        <button>초기화</button>
+
+    const [inputs, setInputs] = useState({name: '', nickname: ''});
+
+    const {name, nickname} = inputs;
+    const nameInput = useRef();
+
+    const onChange = (e) => {
+        const {value, name} = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    }
+    const onReset = (e) => {
+        setInputs({name: '', nickname: ''});
+        nameInput.current.focus();
+
+    }
+    return (
         <div>
-            <b>값 : {text}</b>
+            <input
+                name="name"
+                type="text"
+                placeholder='이름'
+                onChange={onChange}
+                ref={nameInput}
+                value={name}/>
+            <input
+                name="nickname"
+                type="text"
+                placeholder='닉네임'
+                onChange={onChange}
+                value={nickname}/>
+            <button onClick={onReset}>초기화</button>
+            <div>
+                <b>값 :</b>
+                {name}
+                ({nickname})
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default InputSample
