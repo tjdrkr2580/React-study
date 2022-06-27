c5f23ffd8c9cb8c176b8cd5082e27e5c5abe0c36
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-06-27 09:35:31 +0900

    create UserList.jsx : 11. 배열 렌더링하기

--- velopart/src/App.jsx
+++ velopart/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
-import InputSample from './InputSample';
+import UserList from './UserList';
 
 function App() {
   return (
     <>
-    <InputSample/>
+    <UserList/>
     </>
   );
 }
--- velopart/src/UserList.jsx
+++ velopart/src/UserList.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
-const UserList = () => {
+
+const User = ({ user }) => {
   return (
     <div>
-        
+      <b>{user.id} {user.username} </b><span>({user.email})</span>
     </div>
+  )
+}
+
+const UserList = () => {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+
+  ]
+  return (
+    <>
+      {users.map(users => (
+        <User user={users} key={users.id}/>
+      ))}
+    </>
   )
 }
 
